a085931d03ca69eed7258c6337acb81f32733ee8
BHASVIC-sofiacanocarciofa23 <145468173+BHASVIC-sofiacanocarciofa23@users.noreply.github.com>
2024-10-17 15:41:25 +0100

    hello

--- Solitaire Coursework Project/Menu.java
+++ Solitaire Coursework Project/Menu.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class Menu here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -10,12 +10,12 @@ public class Menu extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class Menu.
      * 
      */
     public Menu()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1920, 1080, 1); 
+        super(1920, 400, 1); 
     }
 }
