c1b49afa5888753c0ff29a31f7137c09d19d7f7d
BHASVIC-sofiacanocarciofa23 <sofia.canocarciofa23@students.bhasvic.ac.uk>
2024-10-19 22:08:19 +0100

    lo

--- Menu.java
+++ Menu.java
@@ -1,21 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Menu here.
+ * Where the main menu is generated
  * 
- * @author (your name) 
+ * @sofiacanocarciofa
  * @version (a version number or a date)
  */
 public class Menu extends World
 {
 
-    /**
-     * Constructor for objects of class Menu.
-     * 
+    /* Constructor for objects of class Menu.
      */
     public Menu()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1920, 400, 1); 
+        // Create a new world with 1920x1080 cells with a cell size of 1x1 pixels.
+        super(1920, 1080, 1); 
     }
 }
